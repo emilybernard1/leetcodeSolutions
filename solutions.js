086f784782a6f3dd1c06db222904ac95d9f22123
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2023-05-11 15:41:00 -0500

    twoSum third way

--- solutions.js
+++ solutions.js
@@ -54,3 +54,14 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+var twoSum = function(nums, target){
+    for(let i = 0; i < nums.length; i++) {
+        for(let j= 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
+
