891f78b03ed552775637b1576caea46598d1ad8f
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2023-05-11 15:34:52 -0500

    two sum first way

--- solutions.js
+++ solutions.js
@@ -29,3 +29,18 @@
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+
+// Two Sum 
+// i = 0
+// j = 1
+
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = 1; j < nums.length; j++) {
+            if(nums[i] +nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
